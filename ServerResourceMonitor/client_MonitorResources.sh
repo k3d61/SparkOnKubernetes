5e8b39b1bf89dc3e60d8b89588faf13ae69fcc5a
Kedar Anavardekar <kedar.anavardekar@gmail.com>
2019-03-24 23:21:34 +0530

    Modified client file to store log

--- ServerResourceMonitor/client_MonitorResources.sh
+++ ServerResourceMonitor/client_MonitorResources.sh
@@ -1,10 +1,17 @@
 USAGE=0
 USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
 USAGE="${USAGE//%}"
-echo "$USAGE--$HOSTNAME"
-echo "usage=$USAGE&&hostname=$HOSTNAME"
+echo "$USAGE--$HOSTNAME" >> /tmp/resourceMonotorLog
+echo "usage=$USAGE&&hostname=$HOSTNAME"  >> /tmp/resourceMonotorLog
+
 
 MEMORY_TOTAL=$(free -m | grep Mem | cut -d " " -f9)
 MEMORY_USED=$(free -m | grep Mem | cut -d " " -f14)
 
 curl --request POST --data "usage=$USAGE&&hostname=$HOSTNAME&&memtotal=$MEMORY_TOTAL&&memused=$MEMORY_USED" http://10.16.23.143:8080/resourceMonitor/myresource
+
+
+echo ""e >> /tmp/resourceMonotorLogcho "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"  >> /tmp/resourc
+eMonotorLog
+
+
