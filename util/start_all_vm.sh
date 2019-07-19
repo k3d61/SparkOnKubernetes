8ac4c4a77c39cd8c56ffc184aa758ad52f53f10a
kedar <kedar.anavardekar@gmail.com>
2019-07-19 18:14:57 +0000

    added all VM name to one file

--- util/start_all_vm.sh
+++ util/start_all_vm.sh
@@ -4,7 +4,20 @@
 # This script will be called from cron darmon at every 5 min
 # TODO: read configuration from external .config file 
 
-declare -a VM_NAME=("ub" "spark_1" "spark_2" "spark_3_Cloned" "spark_4" "spark_5")
+declare -a HOST1=("ub" "spark_1" "spark_2" "spark_3_Cloned" "spark_4" "spark_5")
+declare -a HOST2=("spark_6" "spark_7" "spark_8" "spark_9" "spark_10" "spark_11" "spark_12")
+declare -a HOST4=("spark-13" "spark-14" "spark-15" "spark-16" "spark-17" "spark-18")
+
+if [ "$(whoami)" = "ub-01" ];
+then
+	VM_NAME=( "${HOST1[@]}" )
+elif [ "$(whoami)" = "ub-02" ];
+then
+	VM_NAME=( "${HOST2[@]}" )
+elif [ "$(whoami)" = "ub-04" ];
+then
+	VM_NAME=( "${HOST4[@]}" )
+fi
 
 for i in "${VM_NAME[@]}"
 do
