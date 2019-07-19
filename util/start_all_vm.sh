b2d29e773d091a4849086debdebbf19556148173
kedar <kedar.anavardekar@gmail.com>
2019-07-19 13:52:39 +0000

    added some comments

--- util/start_all_vm.sh
+++ util/start_all_vm.sh
@@ -1,6 +1,7 @@
 # Author: Kedar
 # This script checks whether given V 
 # This script creates VMs with static configuration, 
+# This script will be called from cron darmon at every 5 min
 # TODO: read configuration from external .config file 
 
 declare -a VM_NAME=("ub" "spark_1" "spark_2" "spark_3_Cloned" "spark_4" "spark_5")
